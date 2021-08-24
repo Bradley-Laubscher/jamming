49fa90eb7cfc20bb1f729dbc7866538586978a85
Bradley-Laubscher <bradleylaubscher@gmail.com>
2021-08-24 13:37:04 +0200

    Final commit of day1

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Playlist.css'
+
+class Playlist extends React.Component {
+    render() {
+        return (
+            <div className="SearchBar">
+                <input placeholder="Enter A Song, Album, or Artist" />
+                <button className="SearchButton">SEARCH</button>
+            </div>
+        )
+    }
+}
