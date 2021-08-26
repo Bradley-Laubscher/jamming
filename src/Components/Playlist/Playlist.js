7244da5e587e29f44bef7a6d64ac132cfa0182e9
Bradley-Laubscher <bradleylaubscher@gmail.com>
2021-08-26 10:03:54 +0200

    Working - step 39

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './Playlist.css'
+import TrackList from '../TrackList/TrackList'
 
 class Playlist extends React.Component {
     render() {
         return (
-            <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" />
-                <button className="SearchButton">SEARCH</button>
+            <div className="Playlist">
+                <input defaultValue={'New Playlist'}/>
+                <TrackList tracks={this.props.playlistTracks}/>
+                <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
     }
 }
+
+export default Playlist;
