9bf172995ccffce68704b32cd73a8962665b4d12
Bradley-Laubscher <bradleylaubscher@gmail.com>
2021-08-27 09:09:27 +0200

    working 61

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -50,6 +50,7 @@ class App extends React.Component {
 
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -74,6 +75,12 @@ class App extends React.Component {
     });
   }
 
+  updatePlaylistName(name) {
+    this.setState({
+      playlistName: name
+    })
+  }
+
   render() {
     return (
       <div>
@@ -85,7 +92,8 @@ class App extends React.Component {
                            onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName} 
                       playlistTracks={this.state.playlistTracks}
-                      onRemove={this.removeTrack} />
+                      onRemove={this.removeTrack} 
+                      onNameChange={this.updatePlaylistName}/>
           </div>
         </div>
       </div>
